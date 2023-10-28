43faaa448cb164e25bf9da458aa7489045f96570
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 03:45:02 +0530

    Create LoginForm.js

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userCredentials = {
+      email: email,
+      password: password,
+    };
+
+    
+    fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userCredentials),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the login response in this block
+        if (data.accessToken) {
+          // Login successful, save the access token and perform any necessary actions
+          const accessToken = data.accessToken;
+          // For example, you can save the token to local storage or state
+        } else {
+          // Login failed, handle the error, e.g., show an error message
+        }
+      })
+      .catch((error) => {
+        // Handle network or request error, e.g., show an error message
+      });
+  };
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
